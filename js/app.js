85557943a37387811ea0a3085a074ed45ce6742e
Shadaf Hossain <shadafhossain01@gmail.com>
2025-02-01 04:30:43 +0600

    create country.js file

--- js/app.js
+++ js/app.js
@@ -6,12 +6,10 @@ fetch("https://restcountries.com/v3.1/all")
 
 function display(data){
     data.forEach((country)=>{
-        console.log(country);
-        
         countrylists.innerHTML+=`
            <div class="card">
             <a href="/country.html?name=${country.name.common}">
-            <img src="${country.flags.svg}" alt="flg">
+            <img src="${country.flags.svg}" alt="${country.name.common} flg">
             <div class="card-body">
                 <h3>${country.name.common}</h3>
             <p><b>Population: </b>${country.population.toLocaleString()}</p>
